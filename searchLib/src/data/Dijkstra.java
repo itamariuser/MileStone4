502350db8a8e61e0ed24fb3c5fa59fc8129c6547
itama <itama@DESKTOP-HIURG5I>
2017-06-05 17:05:54 +0300

    4matan

--- searchLib/src/data/Dijkstra.java
+++ searchLib/src/data/Dijkstra.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Dijkstra<T> extends CommonSearcher<T> {
 	int numOfEvaluatedNodes;
-	HashSet<State<T>> closed;
+	PriorityQueue<State<T>> closed;
 	HashMap<State<T>,Integer> distances;
 	
 	public Dijkstra() {
 		numOfEvaluatedNodes=0;
-		closed= new HashSet<State<T>>();
+		closed= new PriorityQueue<State<T>>();
 		distances = new HashMap<State<T>,Integer>();
 	}
 	@Override
@@ -24,7 +25,7 @@ public class Dijkstra<T> extends CommonSearcher<T> {
 		while(!openList.isEmpty())
 		{
 			ArrayList<State<T>> neighbors=(ArrayList<State<T>>)s.getAllPossibleStates(openList.pop());
-			discoverAndPutInfinity(neighbors);
+			discoverAndPutInfinity(neighbors);//if visited, what is cost?
 			State<T> minState=chooseCheapestState(neighbors,s);
 			openList.remove(minState);
 			for (State<T> neighborState : neighbors) {
@@ -33,9 +34,17 @@ public class Dijkstra<T> extends CommonSearcher<T> {
 					int alt=distances.get(minState)+ s.getCostBetween(minState, neighborState);
 					if(alt<distances.get(neighborState))
 					{
-						distances.put(neighborState, alt);
 						neighborState.setCameFromState(minState);
+						distances.put(neighborState, alt);
+						neighbors.add(neighborState);
 					}
+				}
+				else
+				{
+					int alt=s.getCostBetween(minState, neighborState);
+					neighborState.setCameFromState(minState);
+					distances.put(neighborState, alt);
+					neighbors.add(neighborState);
 				}
 			}
 			closed.add(minState);
@@ -54,6 +63,10 @@ public class Dijkstra<T> extends CommonSearcher<T> {
 					stateMin=state;
 				}
 			}
+			else 
+			{
+				stateMin=
+			}
 		}
 		return stateMin;
 	}
@@ -66,22 +79,21 @@ public class Dijkstra<T> extends CommonSearcher<T> {
 		numOfEvaluatedNodes++;
 		for (State<T> state : neighbors) {
 			System.out.println(state);
-			if(!openList.contains(state))
+			if(!openList.contains(state) && closed.contains(state))
 			{
 				distances.put(state, Integer.MAX_VALUE);
 				openList.add(state);
 			}
-			else System.out.println("DEILUG");
 		}
 	}
 	@Override
-	public Solution<T> backtrace(State<T> goalState, State<T> initialState) {//TODO: CHANGE HOW BACKTRACE WORKS, GOALSTATE DOESNT HAVE a parent state
+	public Solution<T> backtrace(State<T> goalState, State<T> initialState) {
 		ArrayList<State<T>> pathToVictory = new ArrayList<State<T>>();
 		State<T> tempState = goalState;
-		do {
+		while( tempState!=initialState) {
 			pathToVictory.add(tempState);
 			tempState = tempState.getCameFromState();
-		} while (tempState != initialState);
+		}
 		return new Solution<>(pathToVictory);
 	}
 }
